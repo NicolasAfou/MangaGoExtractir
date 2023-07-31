91c73d81041b629d1de03ec5f6922ac034783a7c
NicolasAfou <nicolas.afournier@proton.me>
2023-07-31 17:50:27 -0400

    finish script & update readme

--- scripts/macInstall.sh
+++ scripts/macInstall.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+
+PROJECT_PATH=$1
+# Check if project path argument is provided
+if [ $# -eq 0 ]; then
+    PROJECT_PATH="/mangaGoExtractor" # to replace if necessary
+fi
+
+
+# Define cleanup function
+cleanup() {
+    echo "An error occurred. Cleaning up..."
+    # Uninstall Node.js and npm
+    brew uninstall node
+    # Uninstall Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+    # Remove all node modules
+    rm -rf "$PROJECT_PATH/node_modules"
+    exit 1
+}
+
+# Trap errors and call cleanup function
+trap 'cleanup' ERR
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -7,7 +30,7 @@
 brew install node
 
 # Change to the project directory
-cd /path/to/project
+cd "$PROJECT_PATH"
 
 # Install dependencies
 npm install
@@ -18,13 +41,5 @@ npm install
 # Run the project
 npm start
 
-# Uninstall Node.js and npm
-brew uninstall node
-
-# Uninstall Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-
-
-# Remove all node modules
-rm -rf /path/to/project/node_modules
-
+# Cleanup on successful completion
+cleanup
