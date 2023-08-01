bac265dd45921532bbf01ad18373e02acc7650f9
NicolasAfou <nicolas.afournier@proton.me>
2023-08-01 17:30:58 -0400

    b

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,3 +1,13 @@
+ #!/bin/bash
+
+
+ PROJECT_PATH=$1
+# Check if project path argument is provided
+if [ $# -eq 0 ]; then
+    PROJECT_PATH="/mangaGoExtractor" # to replace if necessary
+fi
+
+ 
  # Uninstall Node.js and npm
 brew uninstall node
 # Uninstall Homebrew
